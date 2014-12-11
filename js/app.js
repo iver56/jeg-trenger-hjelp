86263c9a6fdae28dcb754c72e557aebc84f7e2c9
iver56 <iver56@hotmail.com>
2014-12-11 12:03:37 +0100

    Remember name and user id. Implement enqueue and dequeue.

--- js/app.js
+++ js/app.js
@@ -1,19 +1,21 @@
-// Ionic Starter App
-
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
 angular.module('jegTrengerHjelp', ['ionic', 'jegTrengerHjelp.controllers', 'firebase'])
 
-.run(function($ionicPlatform) {
+.run(function($ionicPlatform, $rootScope) {
   $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
     if(window.StatusBar) {
       StatusBar.styleDefault();
     }
   });
 });
+
+var guid = (function() {
+  function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  }
+  return function() {
+    return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+      s4() + '-' + s4() + s4() + s4();
+  };
+})();
--- js/controllers.js
+++ js/controllers.js
@@ -1,20 +1,51 @@
 angular.module('jegTrengerHjelp.controllers', [])
 
   .controller('StudentCtrl', function($scope, $firebase, $ionicModal) {
-    $scope.inputNameMode = {value: true};
-    $scope.name = { text: "" };
-    $scope.isHelpNeeded = {value: false};
+    $scope.userId = localStorage.getItem('userId');
+    if (!$scope.userId) {
+      $scope.userId = guid();
+      localStorage.setItem('userId', $scope.userId);
+    }
 
+    $scope.inputNameMode = {value: true};
+    $scope.name = { text: localStorage.getItem('userName') };
+    if ($scope.name.text) {
+      $scope.inputNameMode.value = false;
+    }
     $scope.nameOk = function() {
       $scope.inputNameMode.value = false;
+      localStorage.setItem('userName', $scope.name.text);
     };
 
-    $scope.needHelp = function() {
-      $scope.isHelpNeeded.value = true;
+    $scope.getEnqueuedItemIndex = function() {
+      console.log('checking index...');
+      for (var i = 0; i < $scope.queue.length; i++) {
+        var queueItem = $scope.queue[i];
+        if (queueItem.id === $scope.userId) {
+          return i;
+        }
+      }
+      return -1;
     };
 
-    $scope.neverMind = function() {
-      $scope.isHelpNeeded.value = false;
+    var lastEnqueued = null;
+    $scope.enqueue = function() {
+      if (lastEnqueued && (Date.now() - lastEnqueued) < 300) {
+        return;
+      }
+      if ($scope.getEnqueuedItemIndex() >= 0) {
+        return;
+      }
+      $scope.queue.$add({id: $scope.userId, name: $scope.name.text});
+      lastEnqueued = Date.now();
+    };
+
+    $scope.dequeue = function() {
+      var i = $scope.getEnqueuedItemIndex();
+      if (i >= 0) {
+        var item = $scope.queue[i];
+        $scope.queue.$remove(item);
+      }
     };
 
     $ionicModal.fromTemplateUrl('templates/info-modal.html', {scope: $scope}).then(function(modal) {
